4da7953d26eefde3cc985bd83c667ff91d6a8c23
Lauren Wattendorf <lwattendorf@college.harvard.edu>
2023-02-13 18:21:43 -0500

    comments

--- mongodb-tutorial/index.js
+++ mongodb-tutorial/index.js
@@ -1,16 +1,18 @@
+// MODULE DEPENDENCIES
 const express = require("express");
 const app = express();
 
+// MODULE DEPENDENCIES
 const dotenv = require("dotenv");
 dotenv.config();
 
-//models
+// MODELS
 const TodoTask = require("./models/TodoTask");
 
+// MODULE DEPENDENCIES
 const mongoose = require("mongoose");
-//connection to db
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-    console.log("Connected to db!");
+    // PORT LISTENER
     app.listen(3000, () => console.log("Server Up and running"));
 });
 
@@ -19,11 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-//POST METHOD
+// HTTP METHODS 
 app.post('/',async (req, res) => {
     const todoTask = new TodoTask({
         content: req.body.content
     });
+    // CONTROL FLOW
     try {
         await todoTask.save();
         res.redirect("/");
@@ -32,14 +35,14 @@ app.post('/',async (req, res) => {
     }
 });
 
-// GET METHOD
+// HTTP METHODS 
 app.get("/", (req, res) => {
     TodoTask.find({}, (err, tasks) => {
         res.render("todo.ejs", { todoTasks: tasks });
     });
 });
 
-//UPDATE
+// HTTP METHODS 
 app.route("/edit/:id").get((req, res) => {
     const id = req.params.id;
     TodoTask.find({}, (err, tasks) => {
@@ -54,10 +57,11 @@ app.route("/edit/:id").get((req, res) => {
     });
 });
 
-//DELETE
+// HTTP METHODS 
 app.route("/remove/:id").get((req, res) => {
     const id = req.params.id;
     TodoTask.findByIdAndRemove(id, err => {
+    // CONTROL FLOW
     if (err) return res.send(500, err);
         res.redirect("/");
     });
--- mongodb-tutorial/models/TodoTask.js
+++ mongodb-tutorial/models/TodoTask.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// MODELS
 const todoTaskSchema = new mongoose.Schema({
     content: {
         type: String,
